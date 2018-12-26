41336398a73de8117bb2146a80cbe79679ae5e97
bege13mot <bege13mot@gmail.com>
2018-12-26 19:54:46 +0400

    Add docker

--- backend/main.go
+++ backend/main.go
@@ -4,26 +4,36 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/bege13mot/chat_app/backend/pkg/websocket"
+	"github.com/bege13mot/chat_app/pkg/websocket"
 )
 
 // WebSocket endpoint
-func serveWs(w http.ResponseWriter, r *http.Request) {
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
 
 	// upgrade this connection to WebSocket
-	ws, err := websocket.Upgrade(w, r)
+	conn, err := websocket.Upgrade(w, r)
 
 	if err != nil {
 		fmt.Fprintf(w, "%+v\n", err)
 	}
 
-	//listen indefinitely for new messages coming
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
 }
 
 func main() {
--- backend/pkg/websocket/websocket.go
+++ backend/pkg/websocket/websocket.go
@@ -1,13 +1,10 @@
 package websocket
 
 import (
-	"io"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
-
-	"fmt"
 )
 
 // We will need to define an Upgrader
@@ -26,51 +23,4 @@ func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn, error) {
 		return ws, err
 	}
 	return ws, nil
-}
-
-// define a reader which will listen for new messages
-// being sent to our WebSocket endpoint
-func Reader(conn *websocket.Conn) {
-	for {
-		//read in a message
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		//print message for clarity
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-
-	}
-}
-
-func Writer(conn *websocket.Conn) {
-	for {
-		fmt.Println("Sending")
-		messageType, r, err := conn.NextReader()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		w, err := conn.NextWriter(messageType)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		if _, err := io.Copy(w, r); err != nil {
-			fmt.Println(err)
-			return
-		}
-		if err := w.Close(); err != nil {
-			fmt.Println(err)
-			return
-		}
-
-	}
 }
